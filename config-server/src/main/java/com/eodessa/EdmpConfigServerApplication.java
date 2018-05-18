65ecf38479b82fed175d5f4c89d490ce05033d5c
Sergiy Bruksha <sbruksha@gmail.com>
2018-05-18 09:00:01 -0700

    integration tests added

--- config-server/src/main/java/com/eodessa/EdmpConfigServerApplication.java
+++ config-server/src/main/java/com/eodessa/EdmpConfigServerApplication.java
@@ -1,14 +1,14 @@
 package com.eodessa;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class EdmpConfigServerApplication {
 
 	public static void main(String[] args) {
